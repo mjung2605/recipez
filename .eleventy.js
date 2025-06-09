a169d2ffaab0a888948e2b0911a4705434846f31
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-06-09 21:38:54 +0200

    Deployment vorbereitet

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,15 @@
  * returns all content that has 'type: "Rezept"' in its Front Matter
  */
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addCollection("recipes", function(collectionApi) {
-      const allItems = collectionApi.getAll();
-      return allItems.filter(item => item.data.type === "Rezept");
-    });
+  
+  eleventyConfig.addCollection("recipes", function(collectionApi) {
+    const allItems = collectionApi.getAll();
+    return allItems.filter(item => item.data.type === "Rezept");
+  });
+
+  return {
+    pathPrefix: "/recipez/"
+  };
+
 };
   
